58c8d47fd9c514f225847f11320e368591604b5d
mrethion25 <worldof2512@gmail.com>
2025-11-22 17:28:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,42 @@
 let messageHistory = []; // stores last 100 messages
-const express = require('express')
-const app = express()
-const http = require('http').createServer(app)
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
+// Start server
 http.listen(PORT, () => {
-console.log(Listening on port ${PORT})
-})
-
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', (req, res) => {
-res.sendFile(__dirname + '/index.html')
-})
-
-// Socket
-const io = require('socket.io')(http)
-
-io.on('connection', (socket) => {
-socket.emit("messageHistory", messageHistory);
-console.log('Connected...')
-socket.on("message", (msg) => {
-// save message
-messageHistory.push(msg);
-
-// keep only last 100  
-if (messageHistory.length > 100) {  
-    messageHistory.shift();  
-}  
-
-// broadcast to others  
-socket.broadcast.emit("message", msg);
-
+    console.log(`Listening on port ${PORT}`);
 });
 
-})
+// Serve static files
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Socket.io setup
+const io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+    console.log('Connected…');
+
+    // Send message history to new user
+    socket.emit("messageHistory", messageHistory);
+
+    socket.on("message", (msg) => {
+
+        // Save message
+        messageHistory.push(msg);
+
+        // Keep last 100
+        if (messageHistory.length > 100) {
+            messageHistory.shift();
+        }
+
+        // Broadcast to all EXCEPT sender
+        socket.broadcast.emit("message", msg);
+    });
+});
